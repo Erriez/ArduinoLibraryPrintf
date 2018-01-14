3e6737ebab082667aa8c00dbfa5efd440247f3a9
Erriez <Erriez@users.noreply.github.com>
2018-01-14 22:30:30 +0100

    Fix minor typo in print value.

--- examples/Example/Example.ino
+++ examples/Example/Example.ino
@@ -45,7 +45,7 @@ void setup()
 
   // Use printf() to print the variable
   int value1 = 1234;
-  printf_P(PSTR("millis(): %d\r\n"), value1);
+  printf_P(PSTR("Value: %d\r\n"), value1);
 }
 
 void loop()
