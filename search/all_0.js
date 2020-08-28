20da9143bf7c00b3c7ae7ce9aef00a535c16b69f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-28 15:43:00 +0000

    Deploy Erriez/ErriezPrintf to github.com/Erriez/ErriezPrintf.git:gh-pages

---
+++ search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['erriezprintf_2ecpp',['ErriezPrintf.cpp',['../_erriez_printf_8cpp.html',1,'']]],
+  ['erriezprintf_2eh',['ErriezPrintf.h',['../_erriez_printf_8h.html',1,'']]]
+];
---
+++ search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['printf_28_29_20library_20for_20arduino',['Printf() library for Arduino',['../index.html',1,'']]]
+];
